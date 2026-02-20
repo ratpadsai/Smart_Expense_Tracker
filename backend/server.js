255e5261d477f95646e41f7c1c1d0d5787d58bd6
Sai Vaishnavi <kandukurisaivaishnavi@gmail.com>
2026-02-20 23:21:13 +0530

    Updated server configuration

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://smart-expense-tracker-frontend-sow8.onrender.com",
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
